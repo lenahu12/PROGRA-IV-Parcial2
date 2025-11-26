dcec3b493382e2571e58d968fd514b23e7fcf961
lenahu12 <nahuelquintana97@gmail.com>
2025-11-26 17:34:38 -0300

    modificando para que funcione solo en render

--- parcial/parcial/settings.py
+++ parcial/parcial/settings.py
@@ -91,17 +91,11 @@ WSGI_APPLICATION = 'parcial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if RENDER:
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
